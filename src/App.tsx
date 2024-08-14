29b804cfc59fced728a3b74a495c2df77f64bf1f
Konrad Bonder <Konrad.Bonder@wolterskluwer.com>
2024-08-14 15:47:22 +0200

    feat: add json server and display result on the page

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,33 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Product } from './types';
 
 function App() {
-    return <div className="App">Hello</div>;
+    const [products, setProducts] = useState<Product[]>([]);
+
+    useEffect(() => {
+        fetch('http://localhost:3001/products')
+            .then(res => res.json())
+            .then(data => {
+                setProducts(data);
+            })
+            .catch(error => {
+                console.log(error);
+            });
+    }, []);
+
+    return (
+        <div className="App">
+            <h1>Products</h1>
+            {products.map(product => (
+                <article key={product.title}>
+                    <h4>{product.title}</h4>
+                    <p>{product.price}</p>
+                    <p>{product.discount}</p>
+                    <p>{product.state}</p>
+                </article>
+            ))}
+        </div>
+    );
 }
 
 export default App;
